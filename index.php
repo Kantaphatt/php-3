d5ec751aee7172b473aff13f3aebb6d545b4f795
Kantaphatt <157580646+Kantaphatt@users.noreply.github.com>
2024-07-10 08:09:28 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kantaphatt</title>
+</head>
+<body>
+    <?php
+    $a = 3;
+    $b = 4;
+    echo "<h2>";
+    echo "ผลลัพธ์";
+    echo "</h2>";
+    echo "ค่าของ \$a = 3"."</br>"."ค่าของ \$b = 4";
+    echo "</br>";
+    echo "ผลรวมของ \$a และ \$b "." คือ ".$a+$b;
+    echo "</br>";
+    $A = 20.5;
+    $B = 20.5;
+    $C = $A==$B;
+    echo "<h2>";
+    echo "ผลลัพธ์";
+    echo "</h2>";
+    echo "\$A = B";
+    echo "</br>";
+    echo "\$A = 20.5";
+    echo "</br>";
+    echo "\$B = 20.5";
+    echo "</br>";
+    echo "เท่ากับ ".$C;
+    ?>
+</body>
+</html>
